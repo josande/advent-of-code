62d02f45717dd1f05e069863cfd0da1f9a241ff3
josande <sandell@crashandlearn.se>
2020-12-10 07:07:59 +0100

    2020d10

---
+++ src/main/java/year2020/day10/Day10.java
@@ -0,0 +1,63 @@
+package year2020.day10;
+
+import utils.FileHelper;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day10 {
+
+
+    static Integer solveA(List<Integer> values) {
+        Collections.sort(values);
+        int oneDiff =1, threeDiff=1;
+        for(int i = 1; i<values.size(); i++){
+            if( values.get(i) - values.get(i-1) == 1)
+                oneDiff++;
+            if( values.get(i) - values.get(i-1) == 3)
+                threeDiff++;
+        }
+
+        return oneDiff*threeDiff;
+    }
+
+
+    static Long solveB(List<Integer> values) {
+        Collections.sort(values);
+        HashMap<Integer, Long> pathMap=new HashMap<>();
+        pathMap.put(values.get(values.size()-1)+3, 1L);
+        for(int i = values.size()-1; i>=0; i--) {
+            pathMap.put(values.get(i), findNumberOfChildren(pathMap, values.get(i)));
+        }
+        return findNumberOfChildren(pathMap, 0);
+    }
+
+    private static long findNumberOfChildren(HashMap<Integer, Long> pathMap, int val) {
+        return pathMap.getOrDefault(val+1,0L) +
+               pathMap.getOrDefault(val+2,0L) +
+               pathMap.getOrDefault(val+3,0L);
+
+    }
+
+
+
+    public static void main(String[] args){
+        var day = "Day10";
+
+        var inputs = new FileHelper().readFile("2020/"+day+".txt")
+                .stream().map(Integer::parseInt).collect(Collectors.toList());
+
+        var t0 = System.currentTimeMillis();
+        var ansA = solveA(inputs);
+        var t1 = System.currentTimeMillis();
+        var timePart1 = System.currentTimeMillis()-t0;
+        var ansB = solveB(inputs);
+        var timePart2 = System.currentTimeMillis()-t1;
+
+        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //1656
+        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //56693912375296
+    }
+}
---
+++ src/test/java/year2020/day10/Day10Test.java
@@ -0,0 +1,102 @@
+package year2020.day10;
+
+import org.junit.jupiter.api.Test;
+import year2020.day08.Day08;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day10Test {
+
+    @Test
+    void solveA() {
+        String input =
+                """
+                16
+                10
+                15
+                5
+                1
+                11
+                7
+                19
+                6
+                12
+                4
+                """;
+        List<Integer> values = Arrays.stream(input.split("\n"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+
+        assertEquals(35, Day10.solveA(values));
+    }
+
+    @Test
+    void solveB1() {
+        String input =
+                """
+                16
+                10
+                15
+                5
+                1
+                11
+                7
+                19
+                6
+                12
+                4
+                """;
+        List<Integer> values = Arrays.stream(input.split("\n"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        assertEquals(8, Day10.solveB(values));
+    }
+    @Test
+    void solveB2() {
+        String input =
+                """
+                28
+                33
+                18
+                42
+                31
+                14
+                46
+                20
+                48
+                47
+                24
+                23
+                49
+                45
+                19
+                38
+                39
+                11
+                1
+                32
+                25
+                35
+                8
+                17
+                7
+                9
+                4
+                2
+                34
+                10
+                3
+                """;
+        List<Integer> values = Arrays.stream(input.split("\n"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        assertEquals(19208, Day10.solveB(values));
+    }
+}
