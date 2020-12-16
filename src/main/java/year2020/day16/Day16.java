c35168102a3ff5e394df83ca58487c6aad728247
josande <sandell@crashandlearn.se>
2020-12-16 17:27:03 +0100

    2020d16

---
+++ src/main/java/year2020/day16/Day16.java
@@ -0,0 +1,139 @@
+package year2020.day16;
+
+import utils.FileHelper;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Day16 {
+
+
+    static int solveA(List<String> values) {
+        List<Integer> lowerBounds=new ArrayList<>();
+        List<Integer> upperBounds=new ArrayList<>();
+        for(var v : values) {
+            for(var part : v.split(" ")) {
+                if(part.contains("-"))  {
+                    lowerBounds.add(Integer.parseInt(part.split("-")[0]));
+                    upperBounds.add(Integer.parseInt(part.split("-")[1]));
+                }
+            }
+        }
+        int result=0;
+        int part3=values.indexOf("nearby tickets:")+1;
+        for(int i = part3; i<values.size(); i++) {
+            for(var value : values.get(i).split(",")) {
+                if (!notInAnyRange(Integer.parseInt(value), lowerBounds, upperBounds)) {
+                    result+=Integer.parseInt(value);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static boolean notInAnyRange(Integer value, List<Integer> lowerBounds, List<Integer> upperBounds) {
+        for(int i=0; i<lowerBounds.size(); i++) {
+            if (lowerBounds.get(i) <= value && value <= upperBounds.get(i))
+                return false;
+        }
+        return true;
+    }
+
+    static long solveB(List<String> values) {
+        List<String> names=new ArrayList<>();
+        List<Integer> lowerBounds=new ArrayList<>();
+        List<Integer> upperBounds=new ArrayList<>();
+
+        int part2=values.indexOf("your ticket:");
+
+        for(int i = 0; i<part2; i++) {
+            if(values.get(i).contains(":"))
+                names.add(values.get(i).split(":")[0]);
+
+            for(var part : values.get(i).split(" ")) {
+                if(part.contains("-"))  {
+                    lowerBounds.add(Integer.parseInt(part.split("-")[0]));
+                    upperBounds.add(Integer.parseInt(part.split("-")[1]));
+                }
+            }
+        }
+
+
+        List<Integer> yourTicket = Arrays.stream(values.get(part2+1).split(",")).map(Integer::parseInt).collect(Collectors.toList());
+
+
+        int part3=values.indexOf("nearby tickets:");
+        ArrayList<List<Integer>> tickets = new ArrayList<>();
+        for(int i = part3+1; i<values.size(); i++) {
+            if(allInRange(values.get(i), lowerBounds, upperBounds)) {
+                tickets.add(Arrays.stream(values.get(i).split(",")).map(Integer::parseInt).collect(Collectors.toList()));
+            }
+        }
+        HashMap<String, Integer> namePos = new HashMap<>();
+        while(namePos.size()<names.size()) {
+            for (int pos = 0; pos < tickets.get(0).size(); pos++) {
+                var matches = getMatches(pos, tickets, lowerBounds, upperBounds);
+                if (matches.size() == 1) {
+                    lowerBounds.set(2*matches.get(0), -1);
+                    lowerBounds.set(2*matches.get(0) + 1, -1);
+                    upperBounds.set(2*matches.get(0), -1);
+                    upperBounds.set(2*matches.get(0) + 1, -1);
+                    namePos.put(names.get(matches.get(0)), pos);
+                }
+            }
+        }
+        long result = 1;
+        for(var np : namePos.entrySet()) {
+            if(np.getKey().startsWith("departure")) {
+                result *= yourTicket.get(np.getValue());
+            }
+        }
+        return result;
+
+    }
+
+    static boolean allInRange(String row, List<Integer> lowerBounds, List<Integer> upperBounds) {
+        for(var value : row.split(",")) {
+            if (notInAnyRange(Integer.parseInt(value), lowerBounds, upperBounds)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static List<Integer> getMatches(Integer position, ArrayList<List<Integer>> tickets, List<Integer> lowerBounds, List<Integer> upperBounds) {
+        List<Integer> matches = new ArrayList<>();
+        for(int i=0;i<lowerBounds.size()/2; i++) {
+            boolean match = true;
+            for (var ticket : tickets) {
+                if (!((lowerBounds.get(2*i) <= ticket.get(position) && ticket.get(position)<= upperBounds.get(2*i)) ||
+                    (lowerBounds.get(2*i+1) <= ticket.get(position) && ticket.get(position)<= upperBounds.get(2*i+1))) ) {
+                    match = false;
+                    break;
+                }
+            }
+            if(match)
+                matches.add(i);
+        }
+        return matches;
+    }
+
+
+
+    public static void main (String[] args){
+        var day = "Day16";
+
+        var inputs = new FileHelper().readFile("2020/"+day+".txt");
+
+        var t0 = System.currentTimeMillis();
+        var ansA = solveA(inputs);
+        var t1 = System.currentTimeMillis();
+        var timePart1 = System.currentTimeMillis()-t0;
+        var ansB = solveB(inputs);
+        var timePart2 = System.currentTimeMillis()-t1;
+
+        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //25059
+        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //3253972369789
+    }
+}
---
+++ src/test/java/year2020/day16/Day16Test.java
@@ -0,0 +1,56 @@
+package year2020.day16;
+
+import org.junit.jupiter.api.Test;
+import year2020.day12.Day12;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day16Test {
+
+    @Test
+    void solveA() {
+        String input =
+                """
+                class: 1-3 or 5-7
+                row: 6-11 or 33-44
+                seat: 13-40 or 45-50
+                       
+                your ticket:
+                7,1,14
+                       
+                nearby tickets:
+                7,3,47
+                40,4,50
+                55,2,20
+                38,6,12
+                """;
+        var values = Arrays.stream(input.split("\n")).collect(Collectors.toList());
+
+        assertEquals(71, Day16.solveA(values));
+
+    }
+
+    @Test
+    void solveB() {
+        String input =
+                """
+                class: 0-1 or 4-19
+                departure time: 0-5 or 8-19
+                seat: 0-13 or 16-19
+                
+                your ticket:
+                11,12,13
+                
+                nearby tickets:
+                3,9,18
+                15,1,5
+                5,14,9
+                """;
+        var values = Arrays.stream(input.split("\n")).collect(Collectors.toList());
+
+        assertEquals(11, Day16.solveB(values));
+    }
+}
