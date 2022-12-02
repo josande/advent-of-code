167ad6c5369d216768c98ad20600899c697b2e1a
josande <sandell@crashandlearn.se>
2022-12-02 06:31:57 +0100

    2022 Day2

--- src/main/java/year2022/day02/Day02.java
+++ src/main/java/year2022/day02/Day02.java
@@ -8,13 +8,75 @@ import java.util.List;
 public class Day02 {
 
     static Object solveA(List<String> values) {
+        int score=0;
+        for(String s : values) {
+            char them = s.charAt(0);
+            char you = s.charAt(2);
 
-        return -1;
+            score += getScoreFromChoice(you);
+            score += getScoreFromMatch(them, you);
+        }
+
+        return score;
     }
+
+    private static int getScoreFromChoice(char you) {
+        if (you == 'X') { return 1; }
+        if (you == 'Y') { return 2; }
+        return 3;
+    }
+    static int getScoreFromMatch(char them, char you) {
+        if (you == 'X') {
+            if (them=='A') { return 3; }
+            if (them=='B') {  return 0; }
+            return 6;
+        }
+        if (you == 'Y') {
+            if (them=='B') { return 3; }
+            if (them=='C') { return 0; }
+            return 6;
+        }
+        if (them == 'C') { return 3; }
+        if (them == 'A') { return 0; }
+        return 6;
+    }
+
     static Object solveB(List<String> values) {
+        int score=0;
 
-        return -1;
+        for(String s : values) {
+
+            char them = s.charAt(0);
+            char result = s.charAt(2);
+
+            char you=whatDidYouDo(them, result);
+
+            score += getScoreFromChoice(you);
+            score += getScoreFromMatch(them, you);
+        }
+
+        return score;
     }
+
+
+
+    private static char whatDidYouDo(char them, char result) {
+        if(result == 'X') { //lose
+            if(them == 'A') { return 'Z'; }
+            if (them=='B') { return 'X'; }
+            return 'Y';
+        }
+        if(result == 'Y') { //draw
+            if(them == 'A') { return 'X'; }
+            if (them=='B') { return 'Y'; }
+            return 'Z';
+        }
+        //win
+        if(them == 'A') { return 'Y'; }
+        if (them=='B') { return 'Z'; }
+        return 'X';
+   }
+
     public static void main(String[] args){
         var day = MethodHandles.lookup().lookupClass().getSimpleName();
         var inputs = new FileHelper().readFile("2022/"+day+".txt");
@@ -26,7 +88,7 @@ public class Day02 {
         var timePart1 = t1-t0;
         var timePart2 = System.currentTimeMillis()-t1;
 
-        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //
-        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //
+        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //11873
+        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //12014
     }
 }
--- src/test/java/year2022/day02/Day02Test.java
+++ src/test/java/year2022/day02/Day02Test.java
@@ -12,21 +12,27 @@ class Day02Test {
     @Test
     void testSolveA(){
         String input = """
+                A Y
+                B X
+                C Z
                 """;
         var values = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day02.solveA(values));
+        assertEquals(15, Day02.solveA(values));
     }
     @Test
     void testSolveB() {
         String input = """
+                A Y
+                B X
+                C Z
                 """;
         var values = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day02.solveB(values));
+        assertEquals(12, Day02.solveB(values));
     }
 }
