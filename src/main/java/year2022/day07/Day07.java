bedd5bc028a99a3f833759fb1e989cc5a78a41ff
josande <sandell@crashandlearn.se>
2022-12-07 19:31:02 +0100

    2022 Day7

--- src/main/java/year2022/day07/Day07.java
+++ src/main/java/year2022/day07/Day07.java
@@ -1,19 +1,159 @@
 package year2022.day07;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import utils.FileHelper;
 
 import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Day07 {
+    @AllArgsConstructor
+    @Data
+    static class Folder {
+        String name;
+        List<Folder> folders;
+        List<File> files;
+        Folder parent;
+        Long size;
+
+        public Folder(String name) {
+            this.name=name;
+            folders=new ArrayList<>();
+            files=new ArrayList<>();
+            parent=null;
+        }
+        public void addFolder(Folder folder) {
+            folders.add(folder);
+        }
+        public void addFile(File file) {
+            files.add(file);
+        }
+        public long getSize() {
+            long size = 0L;
+            for(Folder folder : folders) {
+                size+=folder.getSize();
+            }
+            for(File file : files) {
+                size+=file.getSize();
+            }
+            return size;
+        }
+        @Override
+        public int hashCode() {
+            if(parent!=null)
+                return parent.getName().hashCode()+name.hashCode();
+            return "".hashCode()+name.hashCode();
+
+        }
+
+    }
+    @AllArgsConstructor
+    @Data
+    static class File {
+        String name;
+        long size;
+    }
 
     static Object solveA(List<String> values) {
+        Folder rootFolder = new Folder("/");
+        HashSet<Folder> allFolders=new HashSet<>();
+        allFolders.add(rootFolder);
+        Folder currentFolder=rootFolder;
 
-        return -1;
+        for(String s : values) {
+            if(s.startsWith("$")) {
+                if (s.equals("$ cd ..")) {
+                    currentFolder = currentFolder.getParent();
+                } else if (s.startsWith("$ cd ")) {
+                    String name = s.split(" ")[2];
+                    for(Folder c : currentFolder.getFolders()) {
+                        if(c.getName().equals(name)) {
+                            currentFolder=c;
+                        }
+                    }
+                }
+
+            } else {
+
+                if(s.startsWith("dir ")) {
+                    Folder newSubFolder=new Folder(s.split(" ")[1]);
+                    newSubFolder.setParent(currentFolder);
+                    currentFolder.addFolder(newSubFolder);
+                    allFolders.add(newSubFolder);
+
+                } else {
+                    File file = new File(s.split(" ")[1], Long.parseLong(s.split(" ")[0]));
+                    currentFolder.addFile(file);
+                }
+            }
+
+        }
+        int result=0;
+        for(Folder folder : allFolders) {
+            if (folder.getSize() <= 100000L) {
+                result+=folder.getSize();
+            }
+        }
+
+
+
+        return result;
     }
     static Object solveB(List<String> values) {
+        Folder rootFolder = new Folder("/");
+        HashSet<Folder> allFolders=new HashSet<>();
+        allFolders.add(rootFolder);
+        Folder currentFolder=rootFolder;
 
-        return -1;
+        for(String s : values) {
+            if(s.startsWith("$")) {
+                if (s.equals("$ cd ..")) {
+                    currentFolder = currentFolder.getParent();
+                } else if (s.startsWith("$ cd ")) {
+                    String name = s.split(" ")[2];
+                    for(Folder c : currentFolder.getFolders()) {
+                        if(c.getName().equals(name)) {
+                            currentFolder=c;
+                        }
+                    }
+                }
+
+            } else {
+
+                if(s.startsWith("dir ")) {
+                    Folder newSubFolder=new Folder(s.split(" ")[1]);
+                    newSubFolder.setParent(currentFolder);
+                    currentFolder.addFolder(newSubFolder);
+                    allFolders.add(newSubFolder);
+
+                } else {
+                    File file = new File(s.split(" ")[1], Long.parseLong(s.split(" ")[0]));
+                    currentFolder.addFile(file);
+                }
+            }
+
+        }
+
+        long totalSize = 70000000L;
+        long neededSpace = 30000000L;
+        long usedSpace = rootFolder.getSize();
+        long freeSpace=totalSize-usedSpace;
+
+        long missingSpace= neededSpace-freeSpace;
+        System.out.println("usedSpace: "+usedSpace);
+        System.out.println("needed: "+missingSpace);
+
+        long smallestOK=Long.MAX_VALUE;
+        for(Folder folder : allFolders) {
+            if(folder.getSize()>=missingSpace) {
+                smallestOK=Math.min(smallestOK, folder.getSize());
+            }
+        }
+
+            return smallestOK;
     }
     public static void main(String[] args){
         var day = MethodHandles.lookup().lookupClass().getSimpleName();
@@ -26,7 +166,7 @@ public class Day07 {
         var timePart1 = t1-t0;
         var timePart2 = System.currentTimeMillis()-t1;
 
-        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //
-        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //
+        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //1325919
+        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //2050735
     }
 }
--- src/test/java/year2022/day07/Day07Test.java
+++ src/test/java/year2022/day07/Day07Test.java
@@ -12,21 +12,67 @@ class Day07Test {
     @Test
     void testSolveA(){
         String input = """
+                $ cd /
+                $ ls
+                dir a
+                14848514 b.txt
+                8504156 c.dat
+                dir d
+                $ cd a
+                $ ls
+                dir e
+                29116 f
+                2557 g
+                62596 h.lst
+                $ cd e
+                $ ls
+                584 i
+                $ cd ..
+                $ cd ..
+                $ cd d
+                $ ls
+                4060174 j
+                8033020 d.log
+                5626152 d.ext
+                7214296 k
                 """;
         var values = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day07.solveA(values));
+        assertEquals(95437, Day07.solveA(values));
     }
     @Test
     void testSolveB() {
-        String input = """
+        String input =  """
+                $ cd /
+                $ ls
+                dir a
+                14848514 b.txt
+                8504156 c.dat
+                dir d
+                $ cd a
+                $ ls
+                dir e
+                29116 f
+                2557 g
+                62596 h.lst
+                $ cd e
+                $ ls
+                584 i
+                $ cd ..
+                $ cd ..
+                $ cd d
+                $ ls
+                4060174 j
+                8033020 d.log
+                5626152 d.ext
+                7214296 k
                 """;
         var values = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day07.solveB(values));
+        assertEquals(24933642L, Day07.solveB(values));
     }
 }
