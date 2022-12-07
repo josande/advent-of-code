d4d2a3392a0cebbb8781436f4e5057b3339b6ed9
josande <sandell@crashandlearn.se>
2022-12-07 19:28:58 +0100

    2022 Day5

--- src/main/java/year2022/day05/Day05.java
+++ src/main/java/year2022/day05/Day05.java
@@ -3,26 +3,144 @@ package year2022.day05;
 import utils.FileHelper;
 
 import java.lang.invoke.MethodHandles;
-import java.util.List;
+import java.util.*;
 
 public class Day05 {
 
-    static Object solveA(List<String> values) {
+    static Object solveA(int boxes, List<String> values) {
+        HashMap<Integer, Stack<Character>> crates = new HashMap<>();
+        boolean partOne=true;
 
-        return -1;
+        for(int i=0; i<boxes;i++) {
+            crates.put(i+1, new Stack<>());
+        }
+
+        for(String s : values) {
+            String temp=s;
+            if (temp.replaceAll(" ", "").isEmpty()) {
+                partOne = false;
+
+
+                for(Stack<Character> stack : crates.values()) {
+                    reverseStack(stack);
+                }
+                continue;
+
+            }
+
+            if(partOne) {
+                temp = s;
+
+                int box=1;
+                for(int i=0; i<boxes;i++) {
+                    if(temp.length()<i*4+1) continue;
+                    char c = temp.charAt(i*4+1);
+                    if (c != ' ') {
+                        crates.get(box).add(c);
+                    }
+                    box++;
+                }
+            } else {
+                int toMove = Integer.parseInt(s.split(" ")[1]);
+                int fromBox = Integer.parseInt(s.split(" ")[3]);
+                int toBox = Integer.parseInt(s.split(" ")[5]);
+
+                System.out.println(toMove+" "+fromBox+" "+toBox);
+
+                for(int i=0; i<toMove;i++) {
+                    char c = crates.get(fromBox).pop();
+                    crates.get(toBox).add(c);
+                }
+
+            }
+
+        }
+        String result = "";
+        for(int i=1; ;i++) {
+            if(crates.containsKey(i)) {
+                result +=crates.get(i).peek();
+            } else {
+                return result;
+            }
+        }
     }
-    static Object solveB(List<String> values) {
+    static void reverseStack(Stack<Character> stack)  {
+        Queue<Character> queue = new ArrayDeque<>();
+        while (!stack.isEmpty()) {
+            queue.add(stack.pop());
+        }
+        while (!queue.isEmpty()) {
+            stack.add(queue.poll());
+        }
+    }
+    static Object solveB(int boxes, List<String> values) {
+        HashMap<Integer, Stack<Character>> crates = new HashMap<>();
+        boolean partOne=true;
 
-        return -1;
+        for(int i=0; i<boxes;i++) {
+            crates.put(i+1, new Stack<>());
+        }
+
+        for(String s : values) {
+            String temp=s;
+            if (temp.replaceAll(" ", "").isEmpty()) {
+                partOne = false;
+
+
+                for(Stack<Character> stack : crates.values()) {
+                    reverseStack(stack);
+                }
+                continue;
+
+            }
+
+            if(partOne) {
+                temp = s;
+
+                int box=1;
+                for(int i=0; i<boxes;i++) {
+                    if(temp.length()<i*4+1) continue;
+                    char c = temp.charAt(i*4+1);
+                    if (c != ' ') {
+                        crates.get(box).add(c);
+                    }
+                    box++;
+                }
+            } else {
+                int toMove = Integer.parseInt(s.split(" ")[1]);
+                int fromBox = Integer.parseInt(s.split(" ")[3]);
+                int toBox = Integer.parseInt(s.split(" ")[5]);
+
+                Stack<Character> stack = new Stack<>();
+                for(int i=0; i<toMove;i++) {
+                    stack.add(crates.get(fromBox).pop());
+                }
+
+             //   rev  erseStack(stack);
+                while(!stack.isEmpty()) {
+                    crates.get(toBox).add(stack.pop());
+                }
+
+            }
+
+        }
+        String result = "";
+        for(int i=1; ;i++) {
+            if(crates.containsKey(i)) {
+                result +=crates.get(i).peek();
+            } else {
+                return result;
+            }
+        }
     }
     public static void main(String[] args){
         var day = MethodHandles.lookup().lookupClass().getSimpleName();
         var inputs = new FileHelper().readFile("2022/"+day+".txt");
 
         var t0 = System.currentTimeMillis();
-        var ansA = solveA(inputs);
+        var ansA = solveA(9, inputs);
         var t1 = System.currentTimeMillis();
-        var ansB = solveB(inputs);
+        var ansB = solveB(9, inputs);
         var timePart1 = t1-t0;
         var timePart2 = System.currentTimeMillis()-t1;
 
--- src/test/java/year2022/day05/Day05Test.java
+++ src/test/java/year2022/day05/Day05Test.java
@@ -12,21 +12,37 @@ class Day05Test {
     @Test
     void testSolveA(){
         String input = """
+                    [D]   \s
+                [N] [C]   \s
+                [Z] [M] [P]
+                 1   2   3\s
+                                
+                move 1 from 2 to 1
+                move 3 from 1 to 3
+                move 2 from 2 to 1
+                move 1 from 1 to 2
                 """;
         var values = Arrays.stream(input.split("\n"))
-                .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day05.solveA(values));
+        assertEquals("CMZ", Day05.solveA(3, values));
     }
     @Test
     void testSolveB() {
         String input = """
+                    [D]   \s
+                [N] [C]   \s
+                [Z] [M] [P]
+                 1   2   3\s
+                                
+                move 1 from 2 to 1
+                move 3 from 1 to 3
+                move 2 from 2 to 1
+                move 1 from 1 to 2
                 """;
         var values = Arrays.stream(input.split("\n"))
-                .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day05.solveB(values));
+        assertEquals("MCD", Day05.solveB(3, values));
     }
 }
