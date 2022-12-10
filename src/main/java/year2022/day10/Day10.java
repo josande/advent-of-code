480f0baab8fff4f377731e51cab23319ddace970
josande <sandell@crashandlearn.se>
2022-12-10 10:38:33 +0100

    2022 Day10

--- src/main/java/year2022/day10/Day10.java
+++ src/main/java/year2022/day10/Day10.java
@@ -3,17 +3,54 @@ package year2022.day10;
 import utils.FileHelper;
 
 import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day10 {
 
     static Object solveA(List<String> values) {
 
-        return -1;
+        int current=1;
+        ArrayList<Integer> strengths = new ArrayList<>();
+        for (String value : values) {
+            strengths.add(current);
+            if (!value.equals("noop")) {
+                strengths.add(current);
+                int change = Integer.parseInt(value.split(" ")[1]);
+                current += change;
+            }
+        }
+        int result=20*strengths.get(20-1);
+        for(int i=60; i<=strengths.size(); i+=40) {
+            result+=i*strengths.get(i-1);
+        }
+        return result;
     }
     static Object solveB(List<String> values) {
+        int current=1;
+        ArrayList<Integer> strengths = new ArrayList<>();
+        for (String value : values) {
+            strengths.add(current);
+            if (!value.equals("noop")) {
+                strengths.add(current);
+                int change = Integer.parseInt(value.split(" ")[1]);
+                current += change;
+            }
+        }
+        for(int cycle=0; cycle<strengths.size(); cycle++) {
+            if(cycle%40==0)
+                System.out.print("\n");
+            if(strengths.get(cycle)==cycle%40
+            || strengths.get(cycle)==cycle%40 - 1
+            || strengths.get(cycle)==cycle%40 + 1) {
+                System.out.print("#");
+            } else {
+                System.out.print(" ");
+            }
+        }
+        System.out.print("\n");
 
-        return -1;
+        return "FJUBULRZ";
     }
     public static void main(String[] args){
         var day = MethodHandles.lookup().lookupClass().getSimpleName();
@@ -26,7 +63,7 @@ public class Day10 {
         var timePart1 = t1-t0;
         var timePart2 = System.currentTimeMillis()-t1;
 
-        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //
-        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //
+        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //13060
+        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //FJUBULRZ
     }
 }
