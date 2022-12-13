4163ba88f9611c5246976c6bebe2bdccff56739c
josande <sandell@crashandlearn.se>
2022-12-13 21:09:49 +0100

    Day13

--- src/main/java/year2022/day13/Day13.java
+++ src/main/java/year2022/day13/Day13.java
@@ -3,17 +3,121 @@ package year2022.day13;
 import utils.FileHelper;
 
 import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Day13 {
 
+
     static Object solveA(List<String> values) {
+        int matches=0;
+        int index=0;
+        for(int i=0; i<values.size(); i+=3) {
+            String s1=values.get(i);
+            String s2=values.get(i+1);
+            index++;
+            if(compare(s1,s2) < 0) {
+                matches+=index;
+            }
+        }
 
-        return -1;
+        return matches;
     }
-    static Object solveB(List<String> values) {
+    static int compare(String s1, String s2) {
+        if(s1.equals(s2)) {
+            return 0;
+        }
+        if(s1.isEmpty()) {
+            return 1;
+        }
+        if(s2.isEmpty()) {
+            return -1;
+        }
+        if(s1.startsWith("[") && s2.startsWith("[")) {
+            List<String> parts1 = splitToParts(s1);
+            List<String> parts2 = splitToParts(s2);
+            int minLength=Math.min(parts1.size(), parts2.size());
 
-        return -1;
+            for(int j=0; j<minLength; j++) {
+                int result = compare(parts1.get(j), parts2.get(j));
+                if(result!=0) {
+                    return result;
+                }
+            }
+            return parts1.size()-parts2.size();
+        } else if(!s1.startsWith("[") && !s2.startsWith("[")) {
+            return Integer.parseInt(s1)-Integer.parseInt(s2);
+        }
+        else if(s1.startsWith("[")) {
+            s2 = "["+s2+"]";
+        } else {
+            s1 = "["+s1+"]";
+        }
+        return compare(s1, s2);
+
+    }
+
+    static ArrayList<String> splitToParts(String input) {
+        ArrayList<String> parts=new ArrayList<>();
+
+        input = input.substring(1, input.length()-1);
+        while(input.length()>0) {
+            if (input.charAt(0) == '[') {
+                int level=0;
+                for(int i=1; i<input.length(); i++) {
+                    if(input.charAt(i)=='[') {
+                        level ++;
+                    } else if(input.charAt(i) == ']') {
+                        if(level==0) {
+                            parts.add(input.substring(0,i+1));
+                            input=input.substring(i+1);
+                            break;
+                        }
+                        level --;
+                    }
+                }
+            } else if(input.contains(",")) {
+                    parts.add(input.substring(0, input.indexOf(',')));
+                    input=input.substring(input.indexOf(',')+1);
+            } else {
+                parts.add(input);
+                input="";
+            }
+
+        }
+
+        return parts;
+    }
+    static class Part {
+        String input;
+
+    }
+
+
+    static Object solveB(List<String> values) {
+        List<String> packages = new ArrayList<>();
+        for(String s : values) {
+            if(s.isEmpty()) {
+                continue;
+            }
+            packages.add(s);
+        }
+        String divider1="[[2]]";
+        String divider2="[[6]]";
+        packages.add(divider1);
+        packages.add(divider2);
+
+        packages.sort(Day13::compare);
+
+        int result=1;
+        for(int i = 0; i<packages.size(); i++) {
+            if(packages.get(i).equals(divider1) ||
+               packages.get(i).equals(divider2))  {
+                result *=(i+1);
+            }
+        }
+        return result;
     }
     public static void main(String[] args){
         var day = MethodHandles.lookup().lookupClass().getSimpleName();
@@ -26,7 +130,7 @@ public class Day13 {
         var timePart1 = t1-t0;
         var timePart2 = System.currentTimeMillis()-t1;
 
-        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //
-        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //
+        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //5760
+        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //266770
     }
 }
--- src/test/java/year2022/day13/Day13Test.java
+++ src/test/java/year2022/day13/Day13Test.java
@@ -11,22 +11,67 @@ class Day13Test {
 
     @Test
     void testSolveA(){
-        String input = """
+        String input = """                      
+                [1,1,3,1,1]
+                [1,1,5,1,1]
+                
+                [[1],[2,3,4]]
+                [[1],4]
+                
+                [9]
+                [[8,7,6]]
+                
+                [[4,4],4,4]
+                [[4,4],4,4,4]
+                
+                [7,7,7,7]
+                [7,7,7]
+                
+                []
+                [3]
+                
+                [[[]]]
+                [[]]
+                
+                [1,[2,[3,[4,[5,6,7]]]],8,9]
+                [1,[2,[3,[4,[5,6,0]]]],8,9]
                 """;
         var values = Arrays.stream(input.split("\n"))
-                .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day13.solveA(values));
+        assertEquals(13, Day13.solveA(values));
     }
+
     @Test
     void testSolveB() {
-        String input = """
+        String input = """                      
+                [1,1,3,1,1]
+                [1,1,5,1,1]
+                
+                [[1],[2,3,4]]
+                [[1],4]
+                
+                [9]
+                [[8,7,6]]
+                
+                [[4,4],4,4]
+                [[4,4],4,4,4]
+                
+                [7,7,7,7]
+                [7,7,7]
+                
+                []
+                [3]
+                
+                [[[]]]
+                [[]]
+                
+                [1,[2,[3,[4,[5,6,7]]]],8,9]
+                [1,[2,[3,[4,[5,6,0]]]],8,9]
                 """;
         var values = Arrays.stream(input.split("\n"))
-                .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day13.solveB(values));
+        assertEquals(140, Day13.solveB(values));
     }
 }
