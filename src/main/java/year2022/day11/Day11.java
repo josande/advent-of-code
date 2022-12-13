a9aa135815ba8a2564370f6e418076bfd217d8cf
josande <sandell@crashandlearn.se>
2022-12-13 21:05:55 +0100

    2022 Day11

--- src/main/java/year2022/day11/Day11.java
+++ src/main/java/year2022/day11/Day11.java
@@ -1,19 +1,137 @@
 package year2022.day11;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import utils.FileHelper;
 
 import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day11 {
+    @Data
+    @AllArgsConstructor
+    private static class Item {
+        long worryLevel;
+    }
+    @Data
+    private static class Monkey {
+        private ArrayList<Item> items = new ArrayList<>();
+
+        private String[] operation;
+        private String[] test;
+        private int ifTrue;
+        private int ifFalse;
+
+        private int timesInspected;
+
+        public Monkey(String items, String operation, String test, String ifTrue, String ifFalse) {
+            for(String level : items.split(": ")[1].split(", ")) {
+                this.items.add(new Item(Long.parseLong(level)));
+            }
+            this.operation = operation.trim().split(" ");
+            this.test=test.trim().split(" ");
+            this.ifTrue=Integer.parseInt(ifTrue.trim().split(" ")[5]);
+            this.ifFalse=Integer.parseInt(ifFalse.trim().split(" ")[5]);
+        }
+
+        void operation(Item item) {
+            timesInspected++;
+          //  System.out.println(item.getWorryLevel());
+            long val1, val2;
+            if(operation[3].equals("old")) {
+                val1 = item.getWorryLevel();
+            } else {
+                val1 = Long.parseLong(operation[3]);
+            }
+            if(operation[5].equals("old")) {
+                val2 = item.getWorryLevel();
+            } else {
+                val2 = Long.parseLong(operation[5]);
+            }
+            if (operation[4].equals("*")) {
+                item.setWorryLevel(val1 * val2);
+            } else {
+                item.setWorryLevel(val1 + val2);
+            }
+        }
+        void reduceWorry(Item item) {
+            item.setWorryLevel(item.getWorryLevel() / 3L);
+
+        }
+        boolean test(Item item) {
+            long testVal = Long.parseLong(test[3]);
+            return item.getWorryLevel() % testVal == 0;
+        }
+
+        void throwItem(Item item) {
+            if(test(item)) {
+                monkeys.get(ifTrue).getItems().add(item);
+            } else {
+                monkeys.get(ifFalse).getItems().add(item);
+            }
+            items.remove(item);
+        }
+
+    }
+
+    static ArrayList<Monkey> monkeys = new ArrayList<>();
 
     static Object solveA(List<String> values) {
+        monkeys.clear();
 
-        return -1;
+
+        for(int row=0; row<values.size(); row+=7) {
+            monkeys.add(new Monkey(values.get(row+1), 
+                                   values.get(row+2),
+                                   values.get(row+3),
+                                   values.get(row+4),
+                                   values.get(row+5)));
+        }
+        for(int round=0; round <20; round++ ){
+            for(Monkey monkey : monkeys) {
+                while(!monkey.getItems().isEmpty()) {
+                    Item item = monkey.getItems().get(0);
+                    monkey.operation(item);
+                    monkey.reduceWorry(item);
+                    monkey.throwItem(item);
+                }
+            }
+        }
+        List<Integer> timesCounted = monkeys.stream().map(Monkey::getTimesInspected).sorted().collect(Collectors.toList());
+
+        return timesCounted.get(timesCounted.size()-1)*timesCounted.get(timesCounted.size()-2);
     }
     static Object solveB(List<String> values) {
+        monkeys.clear();
 
-        return -1;
+
+        long sanity=1;
+        for(int row=0; row< values.size(); row+=7) {
+            monkeys.add(new Monkey(values.get(row+1),
+                    values.get(row+2),
+                    values.get(row+3),
+                    values.get(row+4),
+                    values.get(row+5)));
+        }
+        for(Monkey n : monkeys) {
+            sanity*=Long.parseLong(n.test[3]);
+        }
+
+        for(int round=0; round < 10000; round++ ){
+              for(Monkey monkey : monkeys) {
+                while(!monkey.getItems().isEmpty()) {
+                    Item item = monkey.getItems().get(0);
+                    monkey.operation(item);
+                    item.setWorryLevel(item.getWorryLevel() % sanity);
+                    monkey.throwItem(item);
+                }
+            }
+        }
+        List<Integer> timesCounted = monkeys.stream().map(Monkey::getTimesInspected).sorted().collect(Collectors.toList());
+
+        return (long) timesCounted.get(timesCounted.size() - 1) *timesCounted.get(timesCounted.size()-2);
     }
     public static void main(String[] args){
         var day = MethodHandles.lookup().lookupClass().getSimpleName();
@@ -26,7 +144,7 @@ public class Day11 {
         var timePart1 = t1-t0;
         var timePart2 = System.currentTimeMillis()-t1;
 
-        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //
-        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //
+        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //54036
+        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //13237873355c
     }
 }
--- src/test/java/year2022/day11/Day11Test.java
+++ src/test/java/year2022/day11/Day11Test.java
@@ -12,21 +12,73 @@ class Day11Test {
     @Test
     void testSolveA(){
         String input = """
+                Monkey 0:
+                  Starting items: 79, 98
+                  Operation: new = old * 19
+                  Test: divisible by 23
+                    If true: throw to monkey 2
+                    If false: throw to monkey 3
+                                
+                Monkey 1:
+                  Starting items: 54, 65, 75, 74
+                  Operation: new = old + 6
+                  Test: divisible by 19
+                    If true: throw to monkey 2
+                    If false: throw to monkey 0
+                                
+                Monkey 2:
+                  Starting items: 79, 60, 97
+                  Operation: new = old * old
+                  Test: divisible by 13
+                    If true: throw to monkey 1
+                    If false: throw to monkey 3
+                                
+                Monkey 3:
+                  Starting items: 74
+                  Operation: new = old + 3
+                  Test: divisible by 17
+                    If true: throw to monkey 0
+                    If false: throw to monkey 1
                 """;
         var values = Arrays.stream(input.split("\n"))
-                .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day11.solveA(values));
+        assertEquals(10605, Day11.solveA(values));
     }
     @Test
     void testSolveB() {
         String input = """
+                Monkey 0:
+                  Starting items: 79, 98
+                  Operation: new = old * 19
+                  Test: divisible by 23
+                    If true: throw to monkey 2
+                    If false: throw to monkey 3
+                                
+                Monkey 1:
+                  Starting items: 54, 65, 75, 74
+                  Operation: new = old + 6
+                  Test: divisible by 19
+                    If true: throw to monkey 2
+                    If false: throw to monkey 0
+                                
+                Monkey 2:
+                  Starting items: 79, 60, 97
+                  Operation: new = old * old
+                  Test: divisible by 13
+                    If true: throw to monkey 1
+                    If false: throw to monkey 3
+                                
+                Monkey 3:
+                  Starting items: 74
+                  Operation: new = old + 3
+                  Test: divisible by 17
+                    If true: throw to monkey 0
+                    If false: throw to monkey 1
                 """;
         var values = Arrays.stream(input.split("\n"))
-                .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day11.solveB(values));
+        assertEquals(2713310158L, Day11.solveB(values));
     }
 }
