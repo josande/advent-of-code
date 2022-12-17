05aebe4e929ee2014249e4f766daea9c9204d218
josande <sandell@crashandlearn.se>
2022-12-17 11:28:36 +0100

    Day15

--- src/main/java/year2022/day15/Day15.java
+++ src/main/java/year2022/day15/Day15.java
@@ -1,32 +1,112 @@
 package year2022.day15;
 
 import utils.FileHelper;
+import utils.MapUtil;
+import utils.Point;
 
 import java.lang.invoke.MethodHandles;
-import java.util.List;
+import java.util.*;
 
 public class Day15 {
 
-    static Object solveA(List<String> values) {
+    static Object solveA(List<String> values, int row) {
+        HashMap<Point, Integer> sensors = new HashMap<>();
+        HashSet<Point> beacons = new HashSet<>();
+        for (String str : values) {
+            String[] parts = str.split(" ");
+            int x1 = Integer.parseInt(parts[2].substring(2, parts[2].length() - 1));
+            int y1 = Integer.parseInt(parts[3].substring(2, parts[3].length() - 1));
+            int x2 = Integer.parseInt(parts[8].substring(2, parts[8].length() - 1));
+            int y2 = Integer.parseInt(parts[9].substring(2, parts[9].length()));
 
-        return -1;
+            Point s = new Point(x1, y1);
+            Point b = new Point(x2, y2);
+            int distance = s.getManhattanDistance(b);
+
+            sensors.put(s, distance);
+            beacons.add(b);
+        }
+
+    int maxDistance = 0;
+
+        for(Map.Entry<Point, Integer> e : sensors.entrySet()) {
+            maxDistance = Math.max(maxDistance, e.getValue());
+        }
+
+
+        int counter=0;
+        for(int x=MapUtil.getMinX(sensors)-maxDistance; x<=MapUtil.getMaxX(sensors)+maxDistance; x++) {
+            boolean tooClose=false;
+            Point p = new Point(x, row);
+            if(beacons.contains(p)) continue;
+            if(sensors.containsKey(p)) continue;
+            for(Map.Entry<Point, Integer> e : sensors.entrySet()) {
+                if(p.getManhattanDistance(e.getKey()) <= e.getValue()) {
+                    tooClose=true;
+                    continue;
+
+                }
+            }
+            if (tooClose) {
+                counter++;
+            }
+        }
+
+        return counter;
     }
-    static Object solveB(List<String> values) {
+    static Object solveB(List<String> values, int max ) {
 
-        return -1;
+
+       HashMap<Point, Integer> sensors = new HashMap<>();
+       HashSet<Point> beacons = new HashSet<>();
+       HashSet<Point> borders = new HashSet<>();
+
+       for (String str : values) {
+           String[] parts = str.split(" ");
+           int x1 = Integer.parseInt(parts[2].substring(2, parts[2].length() - 1));
+           int y1 = Integer.parseInt(parts[3].substring(2, parts[3].length() - 1));
+           int x2 = Integer.parseInt(parts[8].substring(2, parts[8].length() - 1));
+           int y2 = Integer.parseInt(parts[9].substring(2, parts[9].length()));
+
+           Point s = new Point(x1, y1);
+           Point b = new Point(x2, y2);
+           int distance = s.getManhattanDistance(b);
+
+           sensors.put(s, distance);
+           borders.addAll(MapUtil.getPointsAtManhattanDistance(s, distance+1, 0, max, 0, max ));
+        }
+
+
+        for(Point p : borders) {
+            if(beacons.contains(p)) continue;
+            if(sensors.containsKey(p)) continue;
+
+            if(p.getY() >= max || p.getX() >= max || p.getY() < 0 || p.getX() < 0)
+                continue;
+            boolean toClose = false;
+            for(Map.Entry<Point, Integer> e : sensors.entrySet()) {
+                if(p.getManhattanDistance(e.getKey()) <= e.getValue()) {
+                    toClose = true;
+                    break;
+                }
+            }
+            if(!toClose)
+                return (long) p.getX()*4000000+p.getY();
+       }
+       return -1;
     }
     public static void main(String[] args){
         var day = MethodHandles.lookup().lookupClass().getSimpleName();
         var inputs = new FileHelper().readFile("2022/"+day+".txt");
 
         var t0 = System.currentTimeMillis();
-        var ansA = solveA(inputs);
+        var ansA = solveA(inputs,2000000);
         var t1 = System.currentTimeMillis();
-        var ansB = solveB(inputs);
+        var ansB = solveB(inputs, 4000000);
         var timePart1 = t1-t0;
         var timePart2 = System.currentTimeMillis()-t1;
 
-        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //
-        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //
+        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //5688618
+        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //12625383204261
     }
 }
--- src/test/java/year2022/day15/Day15Test.java
+++ src/test/java/year2022/day15/Day15Test.java
@@ -12,21 +12,49 @@ class Day15Test {
     @Test
     void testSolveA(){
         String input = """
+                Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+                Sensor at x=9, y=16: closest beacon is at x=10, y=16
+                Sensor at x=13, y=2: closest beacon is at x=15, y=3
+                Sensor at x=12, y=14: closest beacon is at x=10, y=16
+                Sensor at x=10, y=20: closest beacon is at x=10, y=16
+                Sensor at x=14, y=17: closest beacon is at x=10, y=16
+                Sensor at x=8, y=7: closest beacon is at x=2, y=10
+                Sensor at x=2, y=0: closest beacon is at x=2, y=10
+                Sensor at x=0, y=11: closest beacon is at x=2, y=10
+                Sensor at x=20, y=14: closest beacon is at x=25, y=17
+                Sensor at x=17, y=20: closest beacon is at x=21, y=22
+                Sensor at x=16, y=7: closest beacon is at x=15, y=3
+                Sensor at x=14, y=3: closest beacon is at x=15, y=3
+                Sensor at x=20, y=1: closest beacon is at x=15, y=3
                 """;
         var values = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day15.solveA(values));
+        assertEquals(26, Day15.solveA(values, 10));
     }
     @Test
     void testSolveB() {
         String input = """
+                Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+                Sensor at x=9, y=16: closest beacon is at x=10, y=16
+                Sensor at x=13, y=2: closest beacon is at x=15, y=3
+                Sensor at x=12, y=14: closest beacon is at x=10, y=16
+                Sensor at x=10, y=20: closest beacon is at x=10, y=16
+                Sensor at x=14, y=17: closest beacon is at x=10, y=16
+                Sensor at x=8, y=7: closest beacon is at x=2, y=10
+                Sensor at x=2, y=0: closest beacon is at x=2, y=10
+                Sensor at x=0, y=11: closest beacon is at x=2, y=10
+                Sensor at x=20, y=14: closest beacon is at x=25, y=17
+                Sensor at x=17, y=20: closest beacon is at x=21, y=22
+                Sensor at x=16, y=7: closest beacon is at x=15, y=3
+                Sensor at x=14, y=3: closest beacon is at x=15, y=3
+                Sensor at x=20, y=1: closest beacon is at x=15, y=3
                 """;
         var values = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day15.solveB(values));
+        assertEquals(56000011, Day15.solveB(values, 20));
     }
 }
