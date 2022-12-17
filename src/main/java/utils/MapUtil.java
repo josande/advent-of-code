698b0942727066b70a194be4ee91d5b7d887b89a
josande <sandell@crashandlearn.se>
2022-12-17 12:47:29 +0100

    Day16

--- src/main/java/utils/MapUtil.java
+++ src/main/java/utils/MapUtil.java
@@ -55,26 +55,160 @@ public class MapUtil {
         return map;
     }
     public static void print(HashMap<Point, Character> points) {
-        int maxX= Integer.MIN_VALUE, maxY= Integer.MIN_VALUE, maxZ= Integer.MIN_VALUE,
-                minX= Integer.MAX_VALUE, minY= Integer.MAX_VALUE, minZ= Integer.MAX_VALUE;
-        for ( Point p : points.keySet() ) {
-            maxX=Math.max(maxX, p.getX());
-            maxY=Math.max(maxY, p.getY());
-            maxZ=Math.max(maxZ, p.getZ());
-            minX=Math.min(minX, p.getX());
-            minY=Math.min(minX, p.getY());
-            minZ=Math.min(minZ, p.getZ());
-        }
-
+        int minX = getMinX(points);
+        int maxX = getMaxX(points);
+        int minY = getMinY(points);
+        int maxY = getMaxY(points);
+        int minZ = getMinZ(points);
+        int maxZ = getMaxZ(points);
         for (int z=minZ;z<=maxZ;z++) {
             if(maxZ>0)
                 System.out.println("z="+z);
             for (int y = minY - 1; y <= maxY; y++) {
+                System.out.print(y+"\t");
                 for (int x = minX - 1; x <= maxX; x++) {
                     System.out.print(points.getOrDefault(new Point(x, y, z), ' '));
                 }
                 System.out.print("\n");
             }
         }
+    }
+    public static void print(Collection<Point> points) {
+        int minX = getMinX(points);
+        int maxX = getMaxX(points);
+        int minY = getMinY(points);
+        int maxY = getMaxY(points);
+        int minZ = getMinZ(points);
+        int maxZ = getMaxZ(points);
+        for (int z=minZ;z<=maxZ;z++) {
+            if(maxZ>0)
+                System.out.println("z="+z);
+            for (int y = minY - 1; y <= maxY; y++) {
+                System.out.print(y+"\t");
+                for (int x = minX - 1; x <= maxX; x++) {
+                    if(points.contains(new Point(x,y,z))) {
+                        System.out.print('#');
+
+                    } else {
+                        System.out.print(' ');
+                    }
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+    public static int getMaxX(HashMap<Point, ?> points) {
+        int maxX=Integer.MIN_VALUE;
+        for ( Point p : points.keySet() ) {
+            maxX=Math.max(maxX, p.getX());
+        }
+        return maxX;
+    }
+    public static int getMinX(HashMap<Point, ?> points) {
+        int minX=Integer.MAX_VALUE;
+        for ( Point p : points.keySet() ) {
+            minX=Math.min(minX, p.getX());
+        }
+        return minX;
+    }
+    public static int getMaxY(HashMap<Point, ?> points) {
+        int maxY=Integer.MIN_VALUE;
+        for ( Point p : points.keySet() ) {
+            maxY=Math.max(maxY, p.getY());
+        }
+        return maxY;
+    }
+    public static int getMinY(HashMap<Point, ?> points) {
+        int minY=Integer.MAX_VALUE;
+        for ( Point p : points.keySet() ) {
+            minY=Math.min(minY, p.getY());
+        }
+        return minY;
+    }
+    public static int getMaxZ(HashMap<Point, ?> points) {
+        int maxZ=Integer.MIN_VALUE;
+        for ( Point p : points.keySet() ) {
+            maxZ=Math.max(maxZ, p.getZ());
+        }
+        return maxZ;
+    }
+    public static int getMinZ(HashMap<Point, ?> points) {
+        int minZ=Integer.MAX_VALUE;
+        for ( Point p : points.keySet() ) {
+            minZ=Math.min(minZ, p.getZ());
+        }
+        return minZ;
+    }
+
+    public static int getMaxX(Collection<Point> points) {
+        int maxX=Integer.MIN_VALUE;
+        for ( Point p : points ) {
+            maxX=Math.max(maxX, p.getX());
+        }
+        return maxX;
+    }
+    public static int getMinX(Collection<Point> points) {
+        int minX=Integer.MAX_VALUE;
+        for ( Point p : points ) {
+            minX=Math.min(minX, p.getX());
+        }
+        return minX;
+    }
+    public static int getMaxY(Collection<Point> points) {
+        int maxY=Integer.MIN_VALUE;
+        for ( Point p : points ) {
+            maxY=Math.max(maxY, p.getY());
+        }
+        return maxY;
+    }
+    public static int getMinY(Collection<Point> points) {
+        int minY=Integer.MAX_VALUE;
+        for ( Point p : points ) {
+            minY=Math.min(minY, p.getY());
+        }
+        return minY;
+    }
+    public static int getMaxZ(Collection<Point> points) {
+        int maxZ=Integer.MIN_VALUE;
+        for ( Point p : points) {
+            maxZ=Math.max(maxZ, p.getZ());
+        }
+        return maxZ;
+    }
+    public static int getMinZ(Collection<Point> points) {
+        int minZ=Integer.MAX_VALUE;
+        for ( Point p : points ) {
+            minZ=Math.min(minZ, p.getZ());
+        }
+        return minZ;
+    }
+
+    public static Collection<Point> getPointsAtManhattanDistance(Point p, int distance) {
+        HashSet<Point> points = new HashSet<>();
+
+        for(int i=0; i<=distance; i++) {
+            points.add(new Point(p.getX()-distance+i, p.getY()+i));
+            points.add(new Point(p.getX()-distance+i, p.getY()-i));
+            points.add(new Point(p.getX()+distance-i, p.getY()+i));
+            points.add(new Point(p.getX()+distance-i, p.getY()-i));
+        }
+        return points;
+
+    }
+    public static Collection<Point> getPointsAtManhattanDistance(Point p, int distance, int minX, int maxX, int minY, int maxY) {
+        HashSet<Point> points = new HashSet<>();
+
+        for(int i=0; i<=distance; i++) {
+            if(p.getX()-distance+i >=minX && p.getX()-distance+i <=maxX && p.getY()+i >= minY && p.getY()+i <= maxY)
+                points.add(new Point(p.getX()-distance+i, p.getY()+i));
+            if(p.getX()-distance+i >=minX && p.getX()-distance+i <=maxX && p.getY()-i >= minY && p.getY()-i <= maxY)
+                points.add(new Point(p.getX()-distance+i, p.getY()-i));
+            if(p.getX()+distance-i >=minX && p.getX()+distance-i <=maxX && p.getY()+i >= minY && p.getY()+i <= maxY)
+                points.add(new Point(p.getX()+distance-i, p.getY()+i));
+            if(p.getX()+distance-i >=minX && p.getX()-distance+i <=maxX && p.getY()-i >= minY && p.getY()-i <= maxY)
+                points.add(new Point(p.getX()+distance-i, p.getY()-i));
+        }
+        return points;
+
     }
 }
--- src/test/java/year2022/day16/Day16Test.java
+++ src/test/java/year2022/day16/Day16Test.java
@@ -12,21 +12,41 @@ class Day16Test {
     @Test
     void testSolveA(){
         String input = """
+                Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+                Valve BB has flow rate=13; tunnels lead to valves CC, AA
+                Valve CC has flow rate=2; tunnels lead to valves DD, BB
+                Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+                Valve EE has flow rate=3; tunnels lead to valves FF, DD
+                Valve FF has flow rate=0; tunnels lead to valves EE, GG
+                Valve GG has flow rate=0; tunnels lead to valves FF, HH
+                Valve HH has flow rate=22; tunnel leads to valve GG
+                Valve II has flow rate=0; tunnels lead to valves AA, JJ
+                Valve JJ has flow rate=21; tunnel leads to valve II
                 """;
         var values = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day16.solveA(values));
+        assertEquals(1651, Day16.solveA(values));
     }
     @Test
     void testSolveB() {
         String input = """
+                Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+                Valve BB has flow rate=13; tunnels lead to valves CC, AA
+                Valve CC has flow rate=2; tunnels lead to valves DD, BB
+                Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+                Valve EE has flow rate=3; tunnels lead to valves FF, DD
+                Valve FF has flow rate=0; tunnels lead to valves EE, GG
+                Valve GG has flow rate=0; tunnels lead to valves FF, HH
+                Valve HH has flow rate=22; tunnel leads to valve GG
+                Valve II has flow rate=0; tunnels lead to valves AA, JJ
+                Valve JJ has flow rate=21; tunnel leads to valve II
                 """;
         var values = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day16.solveB(values));
+        assertEquals(1707, Day16.solveB(values));
     }
 }
