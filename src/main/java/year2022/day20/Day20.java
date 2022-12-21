6f857d82d1b96df2beca3b3ff257496458ee4548
josande <sandell@crashandlearn.se>
2022-12-21 14:18:39 +0100

    Day20

--- src/main/java/year2022/day20/Day20.java
+++ src/main/java/year2022/day20/Day20.java
@@ -1,19 +1,106 @@
 package year2022.day20;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import utils.FileHelper;
+import utils.Point;
 
 import java.lang.invoke.MethodHandles;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day20 {
 
-    static Object solveA(List<String> values) {
+    @Data
+    @AllArgsConstructor
+    static class Numb {
+        long value;
+        int index;
 
-        return -1;
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) return false;
+            if (!(obj instanceof Numb)) return false;
+            Numb o = (Numb) obj;
+            return this.value== o.value &&
+                    this.index== o.index;
+        }
+        @Override
+        public int hashCode() {
+            return (int)value*37+index;
+        }
+
+    }
+    static Object solveA(List<String> values) {
+        LinkedList<Numb> numbers = new LinkedList<>();
+        LinkedList<Numb> numbers2 = new LinkedList<>();
+
+        int index=0;
+        for(String s : values) {
+            int number = Integer.parseInt(s);
+            Numb numb = new Numb(number, index++);
+            numbers.add(numb);
+            numbers2.add(numb);
+        }
+
+        for(Numb currentNumb : numbers2){
+
+            int currentIndex=numbers.indexOf(currentNumb);
+            numbers.remove(currentIndex);
+
+            int newIndex = (int) (currentIndex + currentNumb.getValue()% numbers.size() + numbers.size())% numbers.size() ;
+            while(newIndex<0) {
+                newIndex+=numbers.size();
+            }
+
+            numbers.add(newIndex, currentNumb);
+        }
+
+        List<Long> vals = numbers.stream().map(Numb::getValue).collect(Collectors.toList());
+        int indexOfZero= vals.indexOf(0L);
+        long result = vals.get((1000+indexOfZero)%numbers.size());
+        result += vals.get((2000+indexOfZero)%numbers.size());
+        result += vals.get((3000+indexOfZero)%numbers.size());
+
+        return result;
     }
     static Object solveB(List<String> values) {
+        LinkedList<Numb> numbers = new LinkedList<>();
+        LinkedList<Numb> numbers2 = new LinkedList<>();
 
-        return -1;
+        int index=0;
+        long key=811589153L;
+        for(String s : values) {
+            int number = Integer.parseInt(s);
+            Numb numb = new Numb(number*key, index++);
+            numbers.add(numb);
+            numbers2.add(numb);
+        }
+        for(int iteration=0; iteration<10; iteration++) {
+            for (Numb currentNumb : numbers2) {
+
+                int currentIndex = numbers.indexOf(currentNumb);
+                numbers.remove(currentIndex);
+
+                int newIndex = (int) (currentIndex + currentNumb.getValue()% numbers.size() + numbers.size())% numbers.size() ;
+                while (newIndex < 0) {
+                    newIndex += numbers.size();
+                }
+
+                numbers.add(newIndex, currentNumb);
+            }
+            List<Long> vals = numbers.stream().map(Numb::getValue).collect(Collectors.toList());
+        }
+
+        List<Long> vals = numbers.stream().map(Numb::getValue).collect(Collectors.toList());
+        int indexOfZero= vals.indexOf(0L);
+        long result = vals.get((1000+indexOfZero)%numbers.size());
+        result += vals.get((2000+indexOfZero)%numbers.size());
+        result += vals.get((3000+indexOfZero)%numbers.size());
+
+        return result;
     }
     public static void main(String[] args){
         var day = MethodHandles.lookup().lookupClass().getSimpleName();
@@ -26,7 +113,7 @@ public class Day20 {
         var timePart1 = t1-t0;
         var timePart2 = System.currentTimeMillis()-t1;
 
-        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); //
-        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); //
+        System.out.println(day + "A: ("+timePart1+" ms)\t"+ansA); // 4066
+        System.out.println(day + "B: ("+timePart2+" ms)\t"+ansB); // 6704537992933
     }
 }
--- src/test/java/year2022/day20/Day20Test.java
+++ src/test/java/year2022/day20/Day20Test.java
@@ -12,21 +12,35 @@ class Day20Test {
     @Test
     void testSolveA(){
         String input = """
+                1
+                2
+                -3
+                3
+                -2
+                0
+                4
                 """;
         var values = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day20.solveA(values));
+        assertEquals(3L, Day20.solveA(values));
     }
     @Test
     void testSolveB() {
         String input = """
+                1
+                2
+                -3
+                3
+                -2
+                0
+                4
                 """;
         var values = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals(-1, Day20.solveB(values));
+        assertEquals(1623178306L, Day20.solveB(values));
     }
 }
