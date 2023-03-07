52f013a5346c62a4ae519627f619b82ee4c28f57
josande <sandell@crashandlearn.se>
2023-03-07 15:34:22 +0100

    2017:day04

--- src/main/java/year2017/day04/Day04.java
+++ src/main/java/year2017/day04/Day04.java
@@ -3,6 +3,8 @@ package year2017.day04;
 import utils.AdventOfCode;
 import utils.Reporter;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class Day04 implements AdventOfCode {
@@ -12,11 +14,28 @@ public class Day04 implements AdventOfCode {
 
     @Override
     public Object solveA(List<String> input) {
-        return "Not yet implemented";
+        int validCounter=0;
+        for(String row : input) {
+            String[] parts = row.split(" ");
+            HashSet<String> passwords = new HashSet<>(List.of(parts));
+            if(parts.length== passwords.size()) validCounter++;
+        }
+        return validCounter;
     }
 
     @Override
     public Object solveB(List<String> input) {
-        return "Not yet implemented";
+        int validCounter=0;
+        for(String row : input) {
+            HashSet<String> passwords = new HashSet<>();
+            String[] parts = row.split(" ");
+            for(String part:parts) {
+                char[] chars=part.toCharArray();
+                Arrays.sort(chars);
+                passwords.add(new String(chars));
+            }
+            if(parts.length== passwords.size()) validCounter++;
+        }
+        return validCounter;
     }
 }
--- src/test/java/year2017/day04/Day04Test.java
+++ src/test/java/year2017/day04/Day04Test.java
@@ -13,23 +13,29 @@ public class Day04Test {
     @Test
     void testSolveA() {
         String input = """
-
+                aa bb cc dd ee
+                aa bb cc dd aa
+                aa bb cc dd aaa
                 """;
         var inputs = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        Assertions.assertEquals("Not yet implemented", new Day04().solveA(inputs));
+        Assertions.assertEquals(2, new Day04().solveA(inputs));
     }
     @Test
     void testSolveB() {
         String input = """
-
+                abcde fghij
+                abcde xyz ecdab
+                a ab abc abd abf abj
+                iiii oiii ooii oooi oooo
+                oiii ioii iioi iiio
                 """;
         var inputs = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals("Not yet implemented", new Day04().solveB(inputs));
+        assertEquals(3, new Day04().solveB(inputs));
     }
 }
