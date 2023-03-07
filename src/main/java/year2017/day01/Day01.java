dac7b4fb1c0307e93c90cb04c510f8282370919f
josande <sandell@crashandlearn.se>
2023-03-07 13:23:58 +0100

    2017:day01

--- src/main/java/year2017/day01/Day01.java
+++ src/main/java/year2017/day01/Day01.java
@@ -12,11 +12,29 @@ public class Day01 implements AdventOfCode {
 
     @Override
     public Object solveA(List<String> input) {
-        return "Not yet implemented";
+        String digits = input.get(0);
+        int sum=0;
+        int length=digits.length();
+        for(int i=0; i<digits.length();i++) {
+            if(digits.charAt(i) == digits.charAt((i+1)%length)) {
+                sum+=Integer.parseInt(""+digits.charAt(i));
+            }
+        }
+
+        return sum;
     }
 
     @Override
     public Object solveB(List<String> input) {
-        return "Not yet implemented";
+        String digits = input.get(0);
+        int sum=0;
+        int length=digits.length();
+        for(int i=0; i<digits.length();i++) {
+            if(digits.charAt(i) == digits.charAt((i+length/2)%length)) {
+                sum+=Integer.parseInt(""+digits.charAt(i));
+            }
+        }
+
+        return sum;
     }
 }
--- src/test/java/year2017/day01/Day01Test.java
+++ src/test/java/year2017/day01/Day01Test.java
@@ -2,8 +2,7 @@ package year2017.day01;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,24 +10,17 @@ public class Day01Test {
 
     @Test
     void testSolveA() {
-        String input = """
-
-                """;
-        var inputs = Arrays.stream(input.split("\n"))
-                .filter(s->!s.isEmpty())
-                .map(String::valueOf)
-                .collect(Collectors.toList());
-        assertEquals("Not yet implemented", new Day01().solveA(inputs));
+        assertEquals(3, new Day01().solveA(List.of("1122")));
+        assertEquals(4, new Day01().solveA(List.of("1111")));
+        assertEquals(0, new Day01().solveA(List.of("1234")));
+        assertEquals(9, new Day01().solveA(List.of("91212129")));
     }
     @Test
     void testSolveB() {
-        String input = """
-
-                """;
-        var inputs = Arrays.stream(input.split("\n"))
-                .filter(s->!s.isEmpty())
-                .map(String::valueOf)
-                .collect(Collectors.toList());
-        assertEquals("Not yet implemented", new Day01().solveB(inputs));
+        assertEquals(6, new Day01().solveB(List.of("1212")));
+        assertEquals(0, new Day01().solveB(List.of("1221")));
+        assertEquals(4, new Day01().solveB(List.of("123425")));
+        assertEquals(12, new Day01().solveB(List.of("123123")));
+        assertEquals(4, new Day01().solveB(List.of("12131415")));
     }
 }
