fd9a79f8fa09e632a6695de143611fde9ee15bc7
josande <sandell@crashandlearn.se>
2023-03-07 16:16:52 +0100

    2017:day06

--- src/main/java/year2017/day06/Day06.java
+++ src/main/java/year2017/day06/Day06.java
@@ -3,7 +3,7 @@ package year2017.day06;
 import utils.AdventOfCode;
 import utils.Reporter;
 
-import java.util.List;
+import java.util.*;
 
 public class Day06 implements AdventOfCode {
     public static void main(String[] args){
@@ -12,11 +12,48 @@ public class Day06 implements AdventOfCode {
 
     @Override
     public Object solveA(List<String> input) {
-        return "Not yet implemented";
+        ArrayList<Integer> banks = new ArrayList<>();
+        for(String value : input.get(0).split("[ \t]+")) {
+            banks.add(Integer.parseInt(value));
+        }
+        HashSet<ArrayList<Integer>> seenStates = new HashSet<>();
+
+        seenStates.add(banks);
+
+        while(true) {
+            int biggest = Collections.max(banks);
+            int position = banks.indexOf(biggest);
+            banks.set(position,0);
+            for(int i=0; i<biggest;i++) {
+                position=(position+1)%banks.size();
+                banks.set(position, banks.get(position)+1);
+            }
+            if(!seenStates.add(banks)) return seenStates.size();
+        }
     }
 
     @Override
     public Object solveB(List<String> input) {
-        return "Not yet implemented";
+        ArrayList<Integer> banks = new ArrayList<>();
+        for(String value : input.get(0).split("[ \t]+")) {
+            banks.add(Integer.parseInt(value));
+        }
+        HashMap<ArrayList<Integer>, Integer> seenStates = new HashMap<>();
+
+        int steps=0;
+        seenStates.put(banks,steps);
+
+        for(;;steps++) {
+            int biggest = Collections.max(banks);
+            int position = banks.indexOf(biggest);
+            banks.set(position,0);
+            for(int i=0; i<biggest;i++) {
+                position=(position+1)%banks.size();
+                banks.set(position, banks.get(position)+1);
+            }
+            if(seenStates.containsKey(banks))
+                return steps - seenStates.get(banks);
+            seenStates.put(banks, steps);
+        }
     }
 }
--- src/test/java/year2017/day06/Day06Test.java
+++ src/test/java/year2017/day06/Day06Test.java
@@ -13,23 +13,23 @@ public class Day06Test {
     @Test
     void testSolveA() {
         String input = """
-
+                0   2   7   0
                 """;
         var inputs = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        Assertions.assertEquals("Not yet implemented", new Day06().solveA(inputs));
+        Assertions.assertEquals(5, new Day06().solveA(inputs));
     }
     @Test
     void testSolveB() {
         String input = """
-
+                0   2   7   0
                 """;
         var inputs = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals("Not yet implemented", new Day06().solveB(inputs));
+        assertEquals(4, new Day06().solveB(inputs));
     }
 }
