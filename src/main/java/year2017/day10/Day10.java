2e2a2b253be6b63a755c59ace276472f45a800bb
josande <sandell@crashandlearn.se>
2023-03-09 20:50:38 +0100

    2017:day10

--- src/main/java/year2017/day10/Day10.java
+++ src/main/java/year2017/day10/Day10.java
@@ -1,8 +1,11 @@
 package year2017.day10;
 
+import org.apache.commons.lang3.StringUtils;
 import utils.AdventOfCode;
 import utils.Reporter;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Day10 implements AdventOfCode {
@@ -12,11 +15,76 @@ public class Day10 implements AdventOfCode {
 
     @Override
     public Object solveA(List<String> input) {
-        return "Not yet implemented";
+        String[] inputs = input.get(0).split(",");
+        int elements = inputs.length == 4 ?  5 : 256;
+        Integer[] values = new Integer[elements];
+        for(int i=0; i<elements; i++) {
+            values[i]=i;
+        }
+        int position=0, skipSize=0;
+        for(String step : inputs) {
+            int length = Integer.parseInt(step);
+            if(length<=elements) {
+                ArrayList<Integer> toReverse = new ArrayList<>();
+                for (int i = 0; i < length; i++) {
+                    toReverse.add(values[(position + i) % elements]);
+                }
+                Collections.reverse(toReverse);
+                for (int i = 0; i < length; i++) {
+                    values[(position + i) % elements] = toReverse.get(i);
+                }
+
+                position+=length+skipSize;
+                position=position%elements;
+                skipSize++;
+            }
+
+        }
+        return values[0]*values[1];
     }
 
     @Override
     public Object solveB(List<String> input) {
-        return "Not yet implemented";
+
+        ArrayList<Integer> inputs = new ArrayList<>();
+
+        for(char c : input.get(0).toCharArray()) {
+            inputs.add((int) c);
+        }
+        inputs.addAll(List.of(17, 31, 73, 47, 23));
+
+        int elements = 256;
+        Integer[] values = new Integer[elements];
+        for(int i=0; i<elements; i++) {
+            values[i]=i;
+        }
+        int position=0, skipSize=0;
+        for(int round=0; round<64; round++) {
+            for (Integer length : inputs) {
+                if (length <= elements) {
+                    ArrayList<Integer> toReverse = new ArrayList<>();
+                    for (int i = 0; i < length; i++) {
+                        toReverse.add(values[(position + i) % elements]);
+                    }
+                    Collections.reverse(toReverse);
+                    for (int i = 0; i < length; i++) {
+                        values[(position + i) % elements] = toReverse.get(i);
+                    }
+
+                    position += length + skipSize;
+                    position = position % elements;
+                    skipSize++;
+                }
+            }
+        }
+        StringBuilder result= new StringBuilder();
+        for(int i=0; i<16; i++) {
+            int temp=values[i*16];
+            for(int j=1; j<16; j++) {
+                temp^=values[i*16+j];
+            }
+            result.append(StringUtils.leftPad(Integer.toHexString(temp), 2, '0'));
+        }
+        return result.toString();
     }
 }
--- src/test/java/year2017/day10/Day10Test.java
+++ src/test/java/year2017/day10/Day10Test.java
@@ -3,33 +3,19 @@ package year2017.day10;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
 
 public class Day10Test {
 
     @Test
     void testSolveA() {
-        String input = """
-
-                """;
-        var inputs = Arrays.stream(input.split("\n"))
-                .filter(s->!s.isEmpty())
-                .map(String::valueOf)
-                .collect(Collectors.toList());
-        Assertions.assertEquals("Not yet implemented", new Day10().solveA(inputs));
+        Assertions.assertEquals(12, new Day10().solveA(List.of("3,4,1,5")));
     }
     @Test
     void testSolveB() {
-        String input = """
-
-                """;
-        var inputs = Arrays.stream(input.split("\n"))
-                .filter(s->!s.isEmpty())
-                .map(String::valueOf)
-                .collect(Collectors.toList());
-        assertEquals("Not yet implemented", new Day10().solveB(inputs));
+        Assertions.assertEquals("a2582a3a0e66e6e86e3812dcb672a272", new Day10().solveB(List.of("")));
+        Assertions.assertEquals("3efbe78a8d82f29979031a4aa0b16a9d", new Day10().solveB(List.of("1,2,3")));
+        Assertions.assertEquals("63960835bcdc130f0b66d7ff4f6a5a8e", new Day10().solveB(List.of("1,2,4")));
+        Assertions.assertEquals("33efeb34ea91902bb2f59c9920caa6cd", new Day10().solveB(List.of("AoC 2017")));
     }
 }
