00bf6efa97f788a24abca80e151f1f67d5c6a181
josande <sandell@crashandlearn.se>
2023-03-23 08:55:03 +0100

    added aggregated time for 2025

--- src/main/java/year2022/AllOf2022.java
+++ src/main/java/year2022/AllOf2022.java
@@ -1,5 +1,6 @@
 package year2022;
 
+import org.apache.commons.lang3.StringUtils;
 import year2022.day01.Day01;
 import year2022.day02.Day02;
 import year2022.day03.Day03;
@@ -26,9 +27,12 @@ import year2022.day23.Day23;
 import year2022.day24.Day24;
 import year2022.day25.Day25;
 
+import java.lang.invoke.MethodHandles;
+
 @SuppressWarnings("DuplicatedCode")
 public class AllOf2022 {
     public static void main(String[] args){
+        long t0=System.currentTimeMillis();
         Day01.main(null);
         Day02.main(null);
         Day03.main(null);
@@ -54,5 +58,9 @@ public class AllOf2022 {
         Day23.main(null);
         Day24.main(null);
         Day25.main(null);
+        long t1=System.currentTimeMillis();
+        var year = MethodHandles.lookup().lookupClass().getPackageName().substring(4,8);
+        String totalTime = StringUtils.leftPad(""+(t1-t0), 5, '0');
+        System.out.println(year+":Total:  ("+totalTime+ " ms)");
     }
 }
