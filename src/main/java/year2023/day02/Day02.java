3c511bcc60513923d38f82bfd25d4601388e63b7
josande <sandell@crashandlearn.se>
2023-12-02 13:04:37 +0100

    2023-day2

--- src/main/java/year2023/day02/Day02.java
+++ src/main/java/year2023/day02/Day02.java
@@ -12,11 +12,57 @@ public class Day02 implements AdventOfCode {
 
     @Override
     public Object solveA(List<String> input) {
-        return "Not yet implemented";
+        int maxRed =12;
+        int maxGreen =13;
+        int maxBlue =14;
+        int result =0;
+        for(String line : input) {
+            boolean valid=true;
+            for (String set : line.split(": ")[1].split("; ")) {
+                if(valid) {
+                    for(String round : set.split(", ")) {
+                        String[] outcome = round.split(" ");
+                        boolean isOk = switch (outcome[1].trim())  {
+                            case "red" -> Integer.parseInt(outcome[0]) <= maxRed;
+                            case "green" -> Integer.parseInt(outcome[0]) <= maxGreen;
+                            case "blue" -> Integer.parseInt(outcome[0]) <= maxBlue;
+                            default -> throw new IllegalArgumentException();
+                        };
+                        valid &= isOk;
+                    }
+                }
+
+            }
+            if(valid) {
+                result+=Integer.parseInt(line.substring(line.indexOf(" ")+1, line.indexOf(":")));
+            }
+
+        }
+
+        return result;
     }
 
     @Override
     public Object solveB(List<String> input) {
-        return "Not yet implemented";
+        int result =0;
+        for(String line : input) {
+            int minRed =0;
+            int minGreen =0;
+            int minBlue =0;
+            for (String set : line.split(": ")[1].split("; ")) {
+                for(String round : set.split(", ")) {
+                    String[] outcome = round.split(" ");
+                    switch (outcome[1].trim())  {
+                        case "red" -> minRed = Math.max(Integer.parseInt(outcome[0]), minRed);
+                        case "green" -> minGreen = Math.max(Integer.parseInt(outcome[0]), minGreen);
+                        case "blue" -> minBlue = Math.max(Integer.parseInt(outcome[0]), minBlue);
+                        default -> throw new IllegalArgumentException();
+                    }
+                }
+            }
+            result+=minRed * minGreen * minBlue;
+        }
+
+        return result;
     }
 }
--- src/test/java/year2023/day02/Day02Test.java
+++ src/test/java/year2023/day02/Day02Test.java
@@ -12,23 +12,31 @@ public class Day02Test {
     @Test
     void testSolveA() {
         String input = """
-
+                Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
                 """;
         var inputs = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals("Not yet implemented", new Day02().solveA(inputs));
+        assertEquals(8, new Day02().solveA(inputs));
     }
     @Test
     void testSolveB() {
         String input = """
-
+                Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
                 """;
         var inputs = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals("Not yet implemented", new Day02().solveB(inputs));
+        assertEquals(2286, new Day02().solveB(inputs));
     }
 }
