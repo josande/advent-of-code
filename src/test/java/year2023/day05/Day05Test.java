7337c4de15a23a717d95576789c481ec6ef72e0d
johansandell <johan.sandell@nl.abnamro.com>
2023-12-05 09:54:57 +0100

    2023-day05 Slow!

--- src/test/java/year2023/day05/Day05Test.java
+++ src/test/java/year2023/day05/Day05Test.java
@@ -13,23 +13,87 @@ public class Day05Test {
     @Test
     void testSolveA() {
         String input = """
-
+                seeds: 79 14 55 13
+                                
+                seed-to-soil map:
+                50 98 2
+                52 50 48
+                                
+                soil-to-fertilizer map:
+                0 15 37
+                37 52 2
+                39 0 15
+                                
+                fertilizer-to-water map:
+                49 53 8
+                0 11 42
+                42 0 7
+                57 7 4
+                                
+                water-to-light map:
+                88 18 7
+                18 25 70
+                                
+                light-to-temperature map:
+                45 77 23
+                81 45 19
+                68 64 13
+                                
+                temperature-to-humidity map:
+                0 69 1
+                1 0 69
+                                
+                humidity-to-location map:
+                60 56 37
+                56 93 4
                 """;
         var inputs = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        Assertions.assertEquals("Not yet implemented", new Day05().solveA(inputs));
+        Assertions.assertEquals(35L, new Day05().solveA(inputs));
     }
     @Test
     void testSolveB() {
         String input = """
-
+                seeds: 79 14 55 13
+                                
+                seed-to-soil map:
+                50 98 2
+                52 50 48
+                                
+                soil-to-fertilizer map:
+                0 15 37
+                37 52 2
+                39 0 15
+                                
+                fertilizer-to-water map:
+                49 53 8
+                0 11 42
+                42 0 7
+                57 7 4
+                                
+                water-to-light map:
+                88 18 7
+                18 25 70
+                                
+                light-to-temperature map:
+                45 77 23
+                81 45 19
+                68 64 13
+                                
+                temperature-to-humidity map:
+                0 69 1
+                1 0 69
+                                
+                humidity-to-location map:
+                60 56 37
+                56 93 4
                 """;
         var inputs = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals("Not yet implemented", new Day05().solveB(inputs));
+        assertEquals(46L, new Day05().solveB(inputs));
     }
 }
