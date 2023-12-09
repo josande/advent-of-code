276e183686f485b97b190b80ac03d9ccef1dd87b
josande <sandell@crashandlearn.se>
2023-12-09 20:08:18 +0100

    2023-day8

--- src/main/java/year2023/day08/Day08.java
+++ src/main/java/year2023/day08/Day08.java
@@ -1,8 +1,12 @@
 package year2023.day08;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import utils.AdventOfCode;
 import utils.Reporter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Day08 implements AdventOfCode {
@@ -12,11 +16,94 @@ public class Day08 implements AdventOfCode {
 
     @Override
     public Object solveA(List<String> input) {
-        return "Not yet implemented";
+        var maps = new HashMap<String, Pair<String, String>>();
+        String pattern = input.get(0);
+        for(int i=1; i<input.size(); i++) {
+            if(input.get(i).isEmpty()) continue;
+            String[] parts = input.get(i).replaceAll("[(=,)]+", "").split(" +");
+            maps.put(parts[0], new ImmutablePair<>(parts[1], parts[2]));
+        }
+        String current="AAA";
+        for(int steps=0; ; steps++) {
+            if(current.equals("ZZZ")) return steps;
+            if(pattern.charAt(steps%pattern.length()) == 'L')
+                current = maps.get(current).getLeft();
+            else
+                current = maps.get(current).getRight();
+        }
+
     }
 
     @Override
     public Object solveB(List<String> input) {
-        return "Not yet implemented";
+        var maps = new HashMap<String, Pair<String, String>>();
+        String pattern = input.get(0);
+        for(int i=1; i<input.size(); i++) {
+            if(input.get(i).isEmpty()) continue;
+            String[] parts = input.get(i).replaceAll("[(=,)]+", "").split(" +");
+            maps.put(parts[0], new ImmutablePair<>(parts[1], parts[2]));
+        }
+        List<String> current = maps.keySet().stream().filter(c -> c.endsWith("A")).toList();
+
+        List<Pair<Integer, Integer>> cycleLengths = new ArrayList<>();
+        for(String node : current) {
+            cycleLengths.add(getCycleLength(pattern, node, maps));
+        }
+
+        List<List<Integer>> allMatches = new ArrayList<>();
+        for(int i=0; i<current.size(); i++) {
+            allMatches.add(getStepsEndingInZ(current.get(i), cycleLengths.get(i), pattern, maps));
+        }
+
+        int shortestCycle=Integer.MAX_VALUE;
+        int start =0;
+        for(int i=0; i<cycleLengths.size(); i++) {
+            if(cycleLengths.get(i).getLeft()<shortestCycle) {
+                shortestCycle = cycleLengths.get(i).getLeft();
+                start = allMatches.get(i).get(0);
+            }
+        }
+
+        for(long l = start; ;l+=shortestCycle) {
+            boolean allOk=true;
+            for(int i=0; i< cycleLengths.size(); i++) {
+                if ((l - allMatches.get(i).get(allMatches.get(i).size()-1) )% cycleLengths.get(i).getLeft() != 0 ) {
+                    allOk = false;
+                    break;
+                }
+
+            }
+            if (allOk)
+                return l;
+        }
+    }
+    private Pair<Integer, Integer> getCycleLength(String pattern, String start, HashMap<String, Pair<String, String>> maps) {
+        HashMap<Pair<String, Integer>, Integer> positions = new HashMap<>();
+        String current = start;
+        for(int steps=0; ; steps++) {
+            var pos = new ImmutablePair<>(current, steps%pattern.length());
+            if(positions.containsKey(pos)) {
+                int cycleLength = steps-positions.get(pos);
+                int offset = steps-cycleLength;
+                return new ImmutablePair<>(cycleLength, offset);
+            }
+            positions.put(pos, steps);
+
+            if(pattern.charAt(steps%pattern.length()) == 'L')
+                current = maps.get(current).getLeft();
+            else
+                current = maps.get(current).getRight();
+        }
+    }
+    List<Integer> getStepsEndingInZ(String node, Pair<Integer, Integer> cycle, String pattern, HashMap<String, Pair<String, String>> maps) {
+        List<Integer> matches = new ArrayList<>();
+        for(int step=0; step<cycle.getLeft()+cycle.getRight(); step++) {
+            if(step>cycle.getRight() && node.endsWith("Z")) matches.add(step);
+            if(pattern.charAt(step%pattern.length()) == 'L')
+                node = maps.get(node).getLeft();
+            else
+                node = maps.get(node).getRight();
+        }
+        return matches;
     }
 }
--- src/test/java/year2023/day08/Day08Test.java
+++ src/test/java/year2023/day08/Day08Test.java
@@ -11,25 +11,57 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Day08Test {
 
     @Test
-    void testSolveA() {
+    void testSolveA1() {
         String input = """
-
+                RL
+                                
+                AAA = (BBB, CCC)
+                BBB = (DDD, EEE)
+                CCC = (ZZZ, GGG)
+                DDD = (DDD, DDD)
+                EEE = (EEE, EEE)
+                GGG = (GGG, GGG)
+                ZZZ = (ZZZ, ZZZ)
                 """;
         var inputs = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        Assertions.assertEquals("Not yet implemented", new Day08().solveA(inputs));
+        Assertions.assertEquals(2, new Day08().solveA(inputs));
+    }
+    @Test
+    void testSolveA2() {
+        String input = """
+                LLR
+                                
+                AAA = (BBB, BBB)
+                BBB = (AAA, ZZZ)
+                ZZZ = (ZZZ, ZZZ)
+                """;
+        var inputs = Arrays.stream(input.split("\n"))
+                .filter(s->!s.isEmpty())
+                .map(String::valueOf)
+                .collect(Collectors.toList());
+        Assertions.assertEquals(6, new Day08().solveA(inputs));
     }
     @Test
     void testSolveB() {
         String input = """
-
+                LR
+                
+                11A = (11B, XXX)
+                11B = (XXX, 11Z)
+                11Z = (11B, XXX)
+                22A = (22B, XXX)
+                22B = (22C, 22C)
+                22C = (22Z, 22Z)
+                22Z = (22B, 22B)
+                XXX = (XXX, XXX)
                 """;
         var inputs = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals("Not yet implemented", new Day08().solveB(inputs));
+        assertEquals(6L, new Day08().solveB(inputs));
     }
 }
