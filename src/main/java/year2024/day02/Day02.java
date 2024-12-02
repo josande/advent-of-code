59dabd0f583f25606c6f473439671c0be0c19c07
josande <sandell@crashandlearn.nl>
2024-12-02 22:10:49 +0100

    2024: Day02

--- src/main/java/year2024/day02/Day02.java
+++ src/main/java/year2024/day02/Day02.java
@@ -12,11 +12,126 @@ public class Day02 implements AdventOfCode {
 
     @Override
     public Object solveA(List<String> input) {
-        return "Not yet implemented";
+        int safe=0;
+        for(String line : input) {
+            String[] vals = line.split(" ");
+            int lastValue = 0;
+
+            boolean increasing = true;
+            boolean decreasing = true;
+            boolean diff = true;
+            for (String s : vals) {
+                int currentValue = Integer.parseInt(s);
+                if (currentValue > lastValue) {
+                    lastValue = currentValue;
+                } else {
+                    increasing = false;
+                    break;
+                }
+            }
+            lastValue = Integer.MAX_VALUE;
+            for (String s : vals) {
+                int currentValue = Integer.parseInt(s);
+                if (currentValue < lastValue) {
+                    lastValue = currentValue;
+                } else {
+                    decreasing = false;
+                    break;
+                }
+            }
+            lastValue = Integer.parseInt(vals[0]);
+            for (int i = 1; i < vals.length; i++) {
+                int currentValue = Integer.parseInt(vals[i]);
+                if (lastValue == currentValue + 1 ||
+                        lastValue == currentValue + 2 ||
+                        lastValue == currentValue + 3 ||
+                        lastValue == currentValue - 1 ||
+                        lastValue == currentValue - 2 ||
+                        lastValue == currentValue - 3) {
+                    lastValue = currentValue;
+                } else {
+                    diff = false;
+                    break;
+
+                }
+
+
+            }
+            if ((increasing || decreasing) && diff)
+                safe++;
+        }
+        return safe;
+
     }
 
     @Override
     public Object solveB(List<String> input) {
-        return "Not yet implemented";
+        int safe=0;
+        for(String line : input) {
+            String[] vals = line.split(" ");
+            int lastValue;
+            boolean foundSafe=false;
+
+            for (int skipRound = 0; skipRound < vals.length; skipRound++) {
+                boolean increasing = true;
+                boolean decreasing = true;
+                boolean diff = true;
+                if (foundSafe) continue;
+                lastValue=0;
+                for (int r=0; r<vals.length; r++) {
+                    if(r==skipRound) continue;
+                    int currentValue = Integer.parseInt(vals[r]);
+                    if (currentValue > lastValue) {
+                        lastValue = currentValue;
+                    } else {
+                        increasing = false;
+                        break;
+
+                    }
+                }
+                lastValue = Integer.MAX_VALUE;
+                for (int r=0; r<vals.length; r++) {
+                    if(r==skipRound) continue;
+                    int c = Integer.parseInt(vals[r]);
+                    if (c < lastValue) {
+                        lastValue = c;
+                    } else {
+                        decreasing = false;
+                        break;
+                    }
+                }
+                int startAt=1;
+                if(skipRound==0) {
+                    startAt=2;
+                    lastValue = Integer.parseInt(vals[1]);
+
+                } else {
+                    lastValue = Integer.parseInt(vals[0]);
+
+                }
+                for (int i = startAt; i < vals.length; i++) {
+                    if(i==skipRound) continue;
+                    int c = Integer.parseInt(vals[i]);
+                    if (lastValue == c + 1 ||
+                        lastValue == c + 2 ||
+                        lastValue == c + 3 ||
+                        lastValue == c - 1 ||
+                        lastValue == c - 2 ||
+                        lastValue == c - 3) {
+                        lastValue = c;
+                    } else {
+                        diff = false;
+                        break;
+                    }
+                }
+
+                if ((increasing || decreasing) && diff) {
+                    safe++;
+                    foundSafe=true;
+                }
+            }
+        }
+        return safe;
+
     }
 }
--- src/test/java/year2024/day02/Day02Test.java
+++ src/test/java/year2024/day02/Day02Test.java
@@ -12,22 +12,34 @@ public class Day02Test {
     @Test
     void testSolveA() {
         String input = """
+                7 6 4 2 1
+                1 2 7 8 9
+                9 7 6 2 1
+                1 3 2 4 5
+                8 6 4 4 1
+                1 3 6 7 9
                 """;
         var inputs = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
 
-        assertEquals("Not yet implemented", new Day02().solveA(inputs));
+        assertEquals(2, new Day02().solveA(inputs));
     }
     @Test
     void testSolveB() {
         String input = """
+                7 6 4 2 1
+                1 2 7 8 9
+                9 7 6 2 1
+                1 3 2 4 5
+                8 6 4 4 1
+                1 3 6 7 9
                 """;
         var inputs = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals("Not yet implemented", new Day02().solveB(inputs));
+        assertEquals(4, new Day02().solveB(inputs));
     }
 }
