8d5599bcdffd703f0f2ff86cd0afc1fda4b3d0d5
josande <sandell@crashandlearn.nl>
2024-12-07 12:16:47 +0100

    2024: Day07

--- src/main/java/year2024/day07/Day07.java
+++ src/main/java/year2024/day07/Day07.java
@@ -3,6 +3,8 @@ package year2024.day07;
 import utils.AdventOfCode;
 import utils.Reporter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day07 implements AdventOfCode {
@@ -12,11 +14,61 @@ public class Day07 implements AdventOfCode {
 
     @Override
     public Object solveA(List<String> input) {
-        return "Not yet implemented";
+        long result = 0;
+        for (var line : input) {
+            long target = Long.parseLong(line.split(": ")[0]);
+            List<Long> values = Arrays.stream(line.split(": ")[1].split(" "))
+                    .map(Long::parseLong).toList();
+            List<Long> current = new ArrayList<>();
+            for(var value: values)
+                current = getResults(current, value, false, target);
+            if(current.contains(target))
+                result += target;
+        }
+
+
+        return result;
+    }
+
+    private List<Long> getResults(List<Long> current, Long value, boolean partB, Long target) {
+        ArrayList<Long> results = new ArrayList<>();
+        if(current.isEmpty()) {
+            results.add(value);
+            return results;
+        }
+        long result;
+        for(var c : current) {
+            result = c+value;
+            if(result<=target)
+                results.add(result);
+
+            result = c*value;
+            if(result<=target)
+                results.add(result);
+
+            if(partB) {
+                result = Long.parseLong("" + c + value);
+                if(result<=(target))
+                    results.add(result);
+            }
+        }
+        return results;
     }
 
     @Override
     public Object solveB(List<String> input) {
-        return "Not yet implemented";
-    }
+        long result = 0;
+        for (var line : input) {
+            long target = Long.parseLong(line.split(": ")[0]);
+            List<Long> values = Arrays.stream(line.split(": ")[1].split(" "))
+                    .map(Long::parseLong).toList();
+
+            List<Long> current = new ArrayList<>();
+            for(var value: values)
+                current = getResults(current, value, true, target);
+            if(current.contains(target))
+                result += target;
+
+        }
+        return result;    }
 }
--- src/test/java/year2024/day07/Day07Test.java
+++ src/test/java/year2024/day07/Day07Test.java
@@ -12,22 +12,40 @@ public class Day07Test {
     @Test
     void testSolveA() {
         String input = """
+                190: 10 19
+                3267: 81 40 27
+                83: 17 5
+                156: 15 6
+                7290: 6 8 6 15
+                161011: 16 10 13
+                192: 17 8 14
+                21037: 9 7 18 13
+                292: 11 6 16 20
                 """;
         var inputs = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
 
-        assertEquals("Not yet implemented", new Day07().solveA(inputs));
+        assertEquals(3749L, new Day07().solveA(inputs));
     }
     @Test
     void testSolveB() {
         String input = """
+                190: 10 19
+                3267: 81 40 27
+                83: 17 5
+                156: 15 6
+                7290: 6 8 6 15
+                161011: 16 10 13
+                192: 17 8 14
+                21037: 9 7 18 13
+                292: 11 6 16 20
                 """;
         var inputs = Arrays.stream(input.split("\n"))
                 .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals("Not yet implemented", new Day07().solveB(inputs));
+        assertEquals(11387L,  new Day07().solveB(inputs));
     }
 }
