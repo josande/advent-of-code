51cacbc109ce57b2ca4ba6021c1c217c889b6f10
josande <sandell@crashandlearn.nl>
2024-12-19 16:16:46 +0100

    2024: Day19

--- src/main/java/year2024/day19/Day19.java
+++ src/main/java/year2024/day19/Day19.java
@@ -3,20 +3,37 @@ package year2024.day19;
 import utils.AdventOfCode;
 import utils.Reporter;
 
-import java.util.List;
+import java.util.*;
 
 public class Day19 implements AdventOfCode {
     public static void main(String[] args){
+
         Reporter.report(new Day19());
     }
 
+    HashMap<String, Long> knownWays = new HashMap<>();
+
     @Override
     public Object solveA(List<String> input) {
-        return "Not yet implemented";
+        List<String> towels = Arrays.stream(input.getFirst().split(", ")).toList();
+        List<String> designs = new ArrayList<>();
+        for(int i=2; i<input.size(); i++) designs.add(input.get(i));
+        return designs.stream().filter(d->waysToMatch(d, towels)>0).count();
     }
 
     @Override
     public Object solveB(List<String> input) {
-        return "Not yet implemented";
+        List<String> towels = Arrays.stream(input.getFirst().split(", ")).toList();
+        List<String> designs = new ArrayList<>();
+        for(int i=2; i<input.size(); i++) designs.add(input.get(i));
+        return designs.stream().mapToLong(d->waysToMatch(d, towels)).sum();
+    }
+
+    private long waysToMatch(String design, List<String> towels) {
+        if(knownWays.containsKey(design)) return knownWays.get(design);
+        if(design.isEmpty()) return 1;
+        long numberOfWays = towels.stream().filter(design::startsWith).mapToLong(t-> waysToMatch(design.substring(t.length()), towels)).sum();
+        knownWays.put(design, numberOfWays);
+        return numberOfWays;
     }
 }
--- src/test/java/year2024/day19/Day19Test.java
+++ src/test/java/year2024/day19/Day19Test.java
@@ -11,22 +11,40 @@ public class Day19Test {
     @Test
     void testSolveA() {
         String input = """
+                r, wr, b, g, bwu, rb, gb, br
+                
+                brwrr
+                bggr
+                gbbr
+                rrbgbr
+                ubwu
+                bwurrg
+                brgr
+                bbrgwb
                 """;
         var inputs = Arrays.stream(input.split("\n"))
-                .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
 
-        assertEquals("Not yet implemented", new Day19().solveA(inputs));
+        assertEquals(6L, new Day19().solveA(inputs));
     }
     @Test
     void testSolveB() {
         String input = """
+                r, wr, b, g, bwu, rb, gb, br
+                
+                brwrr
+                bggr
+                gbbr
+                rrbgbr
+                ubwu
+                bwurrg
+                brgr
+                bbrgwb
                 """;
         var inputs = Arrays.stream(input.split("\n"))
-                .filter(s->!s.isEmpty())
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        assertEquals("Not yet implemented", new Day19().solveB(inputs));
+        assertEquals(16L, new Day19().solveB(inputs));
     }
 }
