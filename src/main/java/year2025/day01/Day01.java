31ac4e40265e307c2b515f368733bf3d2e7ea6a8
josande <sandell@crashandlearn.nl>
2025-12-01 06:25:04 +0100

    2025: Day1

--- src/main/java/year2025/day01/Day01.java
+++ src/main/java/year2025/day01/Day01.java
@@ -12,11 +12,38 @@ public class Day01 implements AdventOfCode {
 
     @Override
     public Object solveA(List<String> input) {
-        return "Not yet implemented";
+        int result = 0, current = 50;
+        for(String row : input) {
+            boolean left = row.startsWith("L");
+            var value = Integer.parseInt(row.substring(1));
+            if(left) current = (current - value) % 100;
+            if(!left) current = (current + value) % 100;
+            if (current == 0) result++;
+        }
+        return result;
     }
 
     @Override
     public Object solveB(List<String> input) {
-        return "Not yet implemented";
+        int result = 0, current = 50;
+        for(String row : input) {
+            boolean left = row.startsWith("L");
+            var value = Integer.parseInt(row.substring(1));
+            result+=value/100;
+            value%=100;
+            if(value==0) continue;
+            if(left) {
+                var temp =current - value;
+                if (temp<=0) result++;
+                current = (100 + temp) % 100;
+            }
+            if(!left) {
+                var temp =current + value;
+                if (temp>=100) result++;
+                current = (100 + temp) % 100;
+            }
+        }
+
+        return result;
     }
 }
